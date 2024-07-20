bf175a180dcbb5c43194cc0cc7ac0e3648623da1
Vitalii <svika96@gmail.com>
2024-07-20 14:18:40 +0300

    split scss

--- data/js/swiper.js
+++
@@ -1,8 +0,0 @@
-const Swiper = require('swiper').default;
-
-const swiper = new Swiper('.swiper-container', {
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-});
--- gulpfile.js
+++ gulpfile.js
@@ -7,58 +7,45 @@ const browserSync = require("browser-sync").create();
 const autoprefixer = require("gulp-autoprefixer");
 
 
-
-//move CSS 
-function css() {
-	return src('data/styles/css/style.css')
-	  .pipe(dest('app/css'));
-  }
-//
-
 function styles() {
-	return (
-		src("data/styles/scss/style.scss")
-			.pipe(
-				autoprefixer({
-					overrideBrowserlist: ["last 3 version"],
-				})
-			)
-			.pipe(concat("style.min.css"))
-			.pipe(scss({ outputStyle: "compressed" }))
-			// expanded - полностью развёрнутый CSS;
-			// nested - показывает вложенность (по умолчанию);
-			// compact - каждый селектор на новой строке;
-			// compressed - всё в одну строку.
-			.pipe(dest("app/css"))
-			.pipe(browserSync.stream())
-	);
+  return (
+    src("data/styles/*.scss")
+      .pipe(
+        autoprefixer({
+          overrideBrowserlist: ["last 3 version"],
+        })
+      )
+      .pipe(concat("style.min.css"))
+      .pipe(scss({ outputStyle: "compressed" }))
+      // expanded - полностью развёрнутый CSS;
+      // nested - показывает вложенность (по умолчанию);
+      // compact - каждый селектор на новой строке;
+      // compressed - всё в одну строку.
+      .pipe(dest("app/css"))
+      .pipe(browserSync.stream())
+  );
 }
 
-
-
-
-
 function scripts() {
-	return src("data/js/*.js")
-		.pipe(concat("script.min.js"))
-		.pipe(uglify())
-		.pipe(dest("app/js-min"))
-		.pipe(browserSync.stream());
+  return src("data/js/*.js")
+    .pipe(concat("script.min.js"))
+    .pipe(uglify())
+    .pipe(dest("app/js-min"))
+    .pipe(browserSync.stream());
 }
 
 function watching() {
-	watch(["data/styles/css/style.css"], styles);
-	watch(["data/styles/scss/style.scss"], styles);
-	watch(["data/js/script.js"], scripts);
-	watch(["data/*.html"]).on("change", browserSync.reload);
+  watch(["data/styles/*.scss"], styles);
+  watch(["data/js/*.js"], scripts);
+  watch(["./*.html"]).on("change", browserSync.reload);
 }
 
 function browsersync() {
-	browserSync.init({
-		server: {
-			baseDir: "data/",
-		},
-	});
+  browserSync.init({
+    server: {
+      baseDir: "./",
+    },
+  });
 }
 
 exports.styles = styles;
@@ -66,4 +53,4 @@ exports.scripts = scripts;
 exports.watching = watching;
 exports.browsersync = browsersync;
 
-exports.default = parallel(styles, scripts, browsersync, watching, css);
+exports.default = parallel(styles, scripts, browsersync, watching);
