eacb95b2a69d92984b79d215dae81be18df0ea82
Vitalii <svika96@gmail.com>
2024-07-20 16:33:28 +0300

    added swiper cdn

---
+++ data/js/swiper.js
@@ -0,0 +1,11 @@
+    var swiper = new Swiper(".mySwiper", {
+      effect: "flip",
+      grabCursor: true,
+      pagination: {
+        el: ".swiper-pagination",
+      },
+      navigation: {
+        nextEl: ".feature-next",
+        prevEl: ".feature-prev",
+      },
+    });
