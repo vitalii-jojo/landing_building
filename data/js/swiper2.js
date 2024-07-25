98fcc40d96a8704880d498e398ceb2805c7d3ecb
Vitalii <svika96@gmail.com>
2024-07-25 12:28:39 +0300

    fixed second swiper + BTN

--- data/js/swiper2.js
+++ data/js/swiper2.js
@@ -1,13 +1,44 @@
 var swiper2 = new Swiper(".mySwiper2", {
-    slidesPerView: 1,
-    spaceBetween: 30,
-    loop: false,
-    pagination: {
-      el: ".Pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".SwiperBtnNext",
-      prevEl: ".SwiperBtnPrev",
-    },
+  slidesPerView: 1,
+  spaceBetween: 150,
+  loop: false,
+
+  mousewheel: {
+    enabled: true,
+  },
+
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+
+  pagination: {
+    el: ".Pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".SwiperBtnNext",
+    prevEl: ".SwiperBtnPrev",
+  },
+});
+
+// -------
+
+const tabsAll = document.querySelectorAll("[data-tab]");
+const btnAll = document.querySelectorAll("[data-btn]");
+const nextButton = document.querySelector(".SwiperBtnNext");
+const prevButton = document.querySelector(".SwiperBtnPrev");
+
+function updateBtn() {
+  tabsAll.forEach((tab, index) => {
+    if (tab.classList.contains("swiper-slide-active")) {
+      btnAll[index].classList.add("activeb");
+    } else {
+      btnAll[index].classList.remove("activeb");
+    }
+    console.log(tab, index);
   });
+}
+
+nextButton.addEventListener("click", updateBtn);
+prevButton.addEventListener("click", updateBtn);
